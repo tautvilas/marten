3c2b27501c079ed9397a7f425eb21c9df5620f64
carney.fex <carney.fex@673c011e-8dad-11dd-8ab4-67102e0efeef>
2008-10-06 20:14:03 +0000

    Added documentation to all classes of the package. Added an interface for passing simple mathematical functions as parameters of other functions.

--- marten-util/src/main/java/marten/util/Point.java
+++ marten-util/src/main/java/marten/util/Point.java
@@ -1,24 +1,43 @@
 package marten.util;
 
+/**A class to simulate three-dimensional points.
+ * @author Petras Razanskas*/
 public final class Point {
+	/**The origin point.*/
 	public static final Point ZERO = new Point();
-	public double x = 0.0, y = 0.0, z = 0.0;
+	/**The <code>x</code> coordinate of this point.*/
+	public double x = 0.0;
+	/**The <code>y</code> coordinate of this point.*/
+	public double y = 0.0;
+	/**The <code>z</code> coordinate of this point.*/
+	public double z = 0.0;
+	/**An empty constructor, which creates the origin point.*/
 	public Point () {}
+	/**A copy constructor, which creates a point, identical to the <b>source</b> point.
+	 * @param source the source point, copy of which is being made.*/
 	public Point (Point source) {
 		this.x = source.x;
 		this.y = source.y;
 		this.z = source.z;
 	}
+	/**A constructor, which creates a point that would be pointed to by the <b>source</b> vector if it started from the origin.
+	 * @param source the vector, which points to the point being created from the origin.*/
 	public Point (Vector source) {
 		this.x = source.x;
 		this.y = source.y;
 		this.z = source.z;
 	}
+	/**A constructor, which creates a point that is somewhere along the axis of the rotation quaternion centered at the origin.
+	 * @param source the quaternion along the axis of which the point is */
 	public Point (Rotation source) {
 		this.x = source.x;
 		this.y = source.y;
 		this.z = source.z;
 	}
+	/**A constructor, which creates a point from the three given <code>double</code> coordinates.
+	 * @param newX the <code>x</code> coordinate of the new point.
+	 * @param newY the <code>y</code> coordinate of the new point.
+	 * @param newZ the <code>z</code> coordinate of the new point.*/
 	public Point (double newX, double newY, double newZ) {
 		this.x = newX;
 		this.y = newY;
@@ -33,20 +52,26 @@ public final class Point {
 	@Deprecated public void setZ(double z) {
 		this.z = z;
 	}
+	/**Compares the two points for equality.
+	 * @param other the point to which this point is compared to.
+	 * @return <code>true</code> if the points are identical, <code>false</code> otherwise.*/
 	public synchronized boolean equals (Point other) {
 		return this.x == other.x && this.y == other.y && this.z == other.z;
 	}
+	/**Compares the two points for approximate equality.
+	 * @param other the point to which this point is compared to.
+	 * @param epsilon the maximum difference between the respective coordinates for the points to be still considered equal
+	 * @return <code>true</code> if the points are within <b>epsilon</b> length units of each other, <code>false</code> otherwise.
+	 * @throws <code>RuntimeException</code> if the <b>epsilon</b> is negative.*/
 	public synchronized boolean approximatelyEquals (Point other, double epsilon) {
 		if (epsilon < 0.0)
 			throw new RuntimeException ("Attempted approximate comparison with epsilon less than zero.");
-		return (this.x > other.x * (1 - epsilon)) && (this.x < other.x * (1 + epsilon)) && (this.y > other.y * (1 - epsilon)) && (this.y < other.y * (1 + epsilon)) && (this.z > other.z * (1 - epsilon)) && (this.z < other.z * (1 + epsilon)); 			
+		return (Math.abs(this.x - other.x) <= epsilon) && (Math.abs(this.y - other.y) <= epsilon) && (Math.abs(this.z - other.z) <= epsilon); 			
 	}
+	/**@param scaleFactor the <code>double</code> value, signifying how much further than this point the new point will be from the origin.
+	 * @return a point that is in the same direction away from the origin, but <b>scaleFactor</b> times further.*/
 	public synchronized Point scale (double scaleFactor) {
-		Point temp = new Point (this);
-		temp.x *= scaleFactor;
-		temp.y *= scaleFactor;
-		temp.z *= scaleFactor;
-		return temp;
+		return new Point (this.x * scaleFactor, this.y * scaleFactor, this.z * scaleFactor);
 	}
 	@Deprecated public synchronized Point _scale (double scaleFactor) {
 		this.x *= scaleFactor;
@@ -54,19 +79,15 @@ public final class Point {
 		this.z *= scaleFactor;
 		return new Point (this);
 	}
+	/**@param other the vector according to which this point is moved.
+	 * @return a point that is a copy of this point, only moved away in the direction, given by the <b>other</b> vector.*/
 	public synchronized Point move (Vector other) {
-		Point temp = new Point (this);
-		temp.x += other.x;
-		temp.y += other.y;
-		temp.z += other.z;
-		return temp;
+		return new Point (this.x + other.x, this.y + other.y, this.z + other.z);
 	}
+	/**@param other the point to where the origin would be moved.
+	 * @return a point that is a copy of this point when the origin point is moved to the <b>other</b> point.*/
 	public synchronized Point move (Point other) {
-		Point temp = new Point (this);
-		temp.x += other.x;
-		temp.y += other.y;
-		temp.z += other.z;
-		return temp;
+		return new Point (this.x + other.x, this.y + other.y, this.z + other.z);
 	}
 	@Deprecated public synchronized Point _move (Vector other) {
 		this.x += other.x;
@@ -80,18 +101,23 @@ public final class Point {
 		this.z += other.z;
 		return new Point (this);
 	}
+	/**@param other the point to which the distance is measured.
+	 * @return the distance between this and <b>other</b> points. Use {@link #distanceSquared(Point)} if you need this value for comparison since it is calculated faster.*/
 	public synchronized double distance (Point other) {
 		return new Vector(this, other).length();
 	}
+	/**@param other the point to which the distance is measured.
+	 * @return the square of the distance between this and <b>other</b> points.*/
 	public synchronized double distanceSquared (Point other) {
 		return new Vector(this, other).lengthSquared();
 	}
+	/**@param other the rotation pattern, centered around the origin point.
+	 * @return an image of this point as if the coordinate system was rotated around the origin.*/
 	public synchronized Point rotate (Rotation other) {
-		Point temp = new Point ();
-		temp.x = other.w * other.w * this.x + other.x * other.x * this.x + other.y * other.y * this.x - other.z * other.z * this.x + 2 * other.x * other.z * this.z - 2 * other.w * other.z * this.y;
-		temp.y = other.w * other.w * this.y - other.x * other.x * this.y + other.y * other.y * this.y + other.z * other.z * this.y + 2 * other.x * other.y * this.x - 2 * other.w * other.x * this.z;
-		temp.z = other.w * other.w * this.z + other.x * other.x * this.z - other.y * other.y * this.z + other.z * other.z * this.z + 2 * other.y * other.z * this.y - 2 * other.w * other.y * this.x;
-		return temp;
+		Rotation temp = new Rotation (this);
+		temp = other.multiply(temp);
+		temp = temp.multiply(other.inverse());		
+		return new Point(temp);
 	}
 	@Deprecated public synchronized Point _rotate (Rotation other) {
 		Point temp = new Point ();
@@ -103,9 +129,12 @@ public final class Point {
 		this.z = temp.z;
 		return temp;
 	}
+	/**@param other a point to which the mid-point is being calculated.
+	 * @return a point that is right in the middle between this and <b>other</b> points.*/
 	public synchronized Point midPoint (Point other) {
 		return new Point ((this.x + other.x)/2.0, (this.y + other.y)/2.0, (this.z + other.z)/2.0);
 	}
+	/**@return a <code>String</code> representation of this point to facilitate the output.*/
 	@Override public synchronized String toString () {
 		return "(" + this.x + ", " + this.y + ", " + this.z + ")";
 	}
--- marten-util/src/main/java/marten/util/Vector.java
+++ marten-util/src/main/java/marten/util/Vector.java
@@ -1,48 +1,76 @@
 package marten.util;
 
+/**A class to simulate the three-dimensional vectors.
+ * @author Petras Razanskas*/
 public final class Vector {
+	/**A zero vector*/
 	public static final Vector ZERO = new Vector();
-	public double x = 0.0, y = 0.0, z = 0.0;
+	/**The <code>x</code> coordinate of the vector.*/
+	public double x = 0.0;
+	/**The <code>y</code> coordinate of the vector.*/
+	public double y = 0.0;
+	/**The <code>z</code> coordinate of the vector.*/
+	public double z = 0.0;
+	/**An empty constructor which creates a zero vector.*/
 	public Vector () {}
+	/**A copy contructor, which creates an identical vector to the <b>source</b>
+	 * @param source the source vector, copy of which is being made.*/
 	public Vector (Vector source) {
 		this.x = source.x;
 		this.y = source.y;
 		this.z = source.z;
 	}
+	/**A constructor, which creates a vector, which points from the origin point <code>(0,0,0)</code> to the <b>source</b> point.
+	 * @param source the source point, to which the vector being created points.*/
 	public Vector (Point source) {
 		this.x = source.x;
 		this.y = source.y;
 		this.z = source.z;
 	}
+	/**A constructor, which creates a vector, codirectional to the <b>source</b> rotation axis.
+	 * @param source the source rotation, axis of which is used to create this vector.*/
 	public Vector (Rotation source) {
 		this.x = source.x;
 		this.y = source.y;
 		this.z = source.z;
 	}
+	/**A constructor, which creates a vector from the given three <code>double</code> values.
+	 * @param newX the <code>x</code> coordinate of the new vector.
+	 * @param newY the <code>y</code> coordinate of the new vector.
+	 * @param newZ the <code>z</code> coordinate of the new vector.*/
 	public Vector (double newX, double newY, double newZ) {
 		this.x = newX;
 		this.y = newY;
 		this.z = newZ;
 	}
+	/**A constructor, which creates a vector pointing from the <b>start</b> point to the <b>finish</b> point.
+	 * @param start the starting point, from which the vector points.
+	 * @param finish the final point, to which the vector points.*/
 	public Vector (Point start, Point finish) {
 		this.x = finish.x - start.x;
 		this.y = finish.y - start.y;
 		this.z = finish.z - start.z;
 	}
+	/**Compares two vectors for the equality.
+	 * @param other the vector, which is compared to this vector.
+	 * @return <code>true</code> if the vectors are identical, <code>false</code> otherwise.*/
 	public synchronized boolean equals (Vector other) {
 		return (this.x == other.x) && (this.y == other.y) && (this.z == other.z);
 	}
+	/**Compares two vectors for the approximate equality.
+	 * @param other the vector, which is compared to this vector.
+	 * @param epsilon the <code>double</code> value, by which the respective coordinates may be different for the vectors to be still considered equal.
+	 * @return <code>true</code> if the maximum difference between the respective coordinates is less than <b>epsilon</b>, <code>false</code> otherwise.
+	 * @throws <code>RuntimeException</code> when <b>epsilon</b> is negative.*/
 	public synchronized boolean approximatelyEquals (Vector other, double epsilon) {
 		if (epsilon < 0.0)
 			throw new RuntimeException ("Attempted approximate comparison with epsilon less than zero.");
-		return (this.x > other.x * (1 - epsilon)) && (this.x < other.x * (1 + epsilon)) && (this.y > other.y * (1 - epsilon)) && (this.y < other.y * (1 + epsilon)) && (this.z > other.z * (1 - epsilon)) && (this.z < other.z * (1 + epsilon)); 			
+		return (Math.abs(this.x - other.x) <= epsilon) && (Math.abs(this.y - other.y) <= epsilon) && (Math.abs(this.z - other.z) <= epsilon); 			
 	}
+	/**@param scaleFactor the <code>double</code> value, signifying how much the resulting vector will be longer than this vector.
+	 * @return a vector, which is collinear to this vector, only <b>scaleFactor</b> times longer. The vectors are codirectional if the <b>scaleFactor</b> is positive, opposite otherwise.*/
 	public synchronized Vector scale (double scaleFactor) {
-		Vector temp = new Vector (this);
-		temp.x *= scaleFactor;
-		temp.y *= scaleFactor;
-		temp.z *= scaleFactor;
-		return temp;
+		return new Vector (this.x * scaleFactor, this.y * scaleFactor, this.z * scaleFactor);
 	}
 	@Deprecated public synchronized Vector _scale (double scaleFactor) {
 		this.x *= scaleFactor;
@@ -50,12 +78,12 @@ public final class Vector {
 		this.z *= scaleFactor;
 		return new Vector (this);
 	}
+	/**@param scaleX the warp factor along <code>x</code> axis.
+	 * @param scaleY the warp factor along <code>y</code> axis.
+	 * @param scaleZ the warp factor along <code>z</code> axis.
+	 * @return a vector, which is warped along the coordinate axis.*/
 	public synchronized Vector scaleAssymetrically (double scaleX, double scaleY, double scaleZ) {
-		Vector temp = new Vector (this);
-		temp.x *= scaleX;
-		temp.y *= scaleY;
-		temp.z *= scaleZ;
-		return temp;
+		return new Vector (this.x * scaleX, this.y * scaleY, this.z * scaleZ);
 	}
 	@Deprecated public synchronized Vector _scaleAssymetrically (double scaleX, double scaleY, double scaleZ) {
 		this.x *= scaleX;
@@ -63,12 +91,10 @@ public final class Vector {
 		this.z *= scaleZ;
 		return new Vector (this);
 	}
+	/**@param other the vector, which is added to this vector.
+	 * @return the sum of this and <b>other</b> vectors.*/
 	public synchronized Vector add (Vector other) {
-		Vector temp = new Vector (this);
-		temp.x += other.x;
-		temp.y += other.y;
-		temp.z += other.z;
-		return temp;
+		return new Vector (this.x + other.x, this.y + other.y, this.z + other.z);
 	}
 	@Deprecated public synchronized Vector _add (Vector other) {
 		this.x += other.x;
@@ -76,21 +102,23 @@ public final class Vector {
 		this.z += other.z;
 		return new Vector (this);
 	}
+	/**@param other the vector, which is dot-multiplied with this vector.
+	 * @return the dot-product of this and <b>other</b> vectors.*/
 	public synchronized double dot (Vector other) {
 		return this.x * other.x + this.y * other.y + this.z * other.z;	
 	}
+	/**@return the square of the length of this vector.*/
 	public synchronized double lengthSquared () {
 		return this.dot(this);
 	}
+	/**@return the actual length of this vector. Use {@link #lengthSquared()} if you need this value for comparison since it is calculated more effectively.*/
 	public synchronized double length () {
 		return Math.sqrt(this.lengthSquared());
 	}
+	/**@param other the vector which is cross-multiplied with this vector.
+	 * @return the cross product of this and <b>other</b> vectors.*/
 	public synchronized Vector cross (Vector other) {
-		Vector temp = new Vector ();
-		temp.x = this.y * other.z - this.z * other.y;
-		temp.y = this.z * other.x - this.x * other.z;
-		temp.z = this.x * other.y - this.y * other.x;
-		return temp;
+		return new Vector (this.y * other.z - this.z * other.y, this.z * other.x - this.x * other.z, this.x * other.y - this.y * other.x);
 	}
 	@Deprecated public synchronized Vector _cross (Vector other) {
 		Vector temp = new Vector ();
@@ -102,6 +130,8 @@ public final class Vector {
 		this.z = temp.z;
 		return temp;
 	}
+	/**@param rotation the quaternion that defines, how this vector should be rotated.
+	 * @return the rotated vector.*/
 	public synchronized Vector rotate (Rotation other) {
 		Rotation temp = new Rotation (this);
 		temp = other.multiply(temp);
@@ -117,6 +147,7 @@ public final class Vector {
 		this.z = temp.z;
 		return new Vector(temp.x, temp.y, temp.z);
 	}
+	/**@return the vector with length of 1 and codirectional with this vector.*/
 	public synchronized Vector normalize() {
 		double length = this.length();
 		if (length == 0.0)
@@ -129,15 +160,19 @@ public final class Vector {
 			return new Vector();
 		return this._scale(1 / length);	
 	}
+	/**@return a vector that is of the same length but points in an exactly opposite direction than this vector.*/
 	public synchronized Vector negate() {
 		return this.scale(-1.0);
 	}
 	@Deprecated public synchronized Vector _negate() {
 		return this._scale(-1.0);
 	}
+	/**@param other a vector to which the angle is measured.
+	 * @return the angle between this and <b>other</b> vectors as a <code>double</code> value in radians.*/
 	public synchronized double angle (Vector other) {
 		return Math.acos(this.normalize().dot(other.normalize()));
 	}
+	/**@return a string representation of this vector to facilitate the output.*/
 	@Override public synchronized String toString () {
 		return "(" + this.x + ", " + this.y + ", " + this.z + ")";
 	}
