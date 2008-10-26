c13d79cfe54fd76712e57a6a3d5b758f5e95b264
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2008-10-26 12:00:10 +0000

    Made general AgeFeature test class for simple feature testing. Added Model loading feature test Removed Hud test

--- marten-testing/src/main/java/marten/testing/tests/AgeFeatureTest.java
+++ marten-testing/src/main/java/marten/testing/tests/AgeFeatureTest.java
@@ -1,12 +1,16 @@
 package marten.testing.tests;
 
+import org.lwjgl.opengl.Display;
+
 import marten.age.AgeApp;
 import marten.age.BitmapFont;
 import marten.age.BitmapString;
 import marten.age.Camera;
+import marten.age.ComplexModel;
 import marten.age.FontCache;
 import marten.age.FrustumCamera;
 import marten.age.Hud;
+import marten.age.ModelLoader;
 import marten.age.SimpleModel;
 import marten.age.SimpleRoot;
 import marten.age.geometry.OptimizedGeometry;
@@ -17,59 +21,85 @@ import marten.age.util.FpsCounter;
 import marten.util.Point;
 import marten.util.Rotation;
 
-import org.lwjgl.opengl.Display;
-
-
-public class HudTest extends AgeApp {
+public class AgeFeatureTest extends AgeApp {
 	
 	private SimpleRoot sr;
-	
+
 	@Override
 	protected void init() {
+		/* Scene Init */
 		sr = new SimpleRoot();
-		Hud hud = new Hud();
-		
-		SimpleModel sm = new SimpleModel(new OptimizedGeometry(new SphereGeometry(2.0)));
 		
 		Camera frontCamera = new FrustumCamera();
 		frontCamera.setSettings(new Point(), new Rotation(), -10.0);
 		sr.addCamera("front", frontCamera);
 		sr.setActiveCamera("front");
+		
+		/* Hud */
+		
+		Hud hud = new Hud();
 		sr.addHud("mainhud", hud);
 		sr.setActiveHud("mainhud");
+		
+		/* Bitmap Font */
 		
 		FontCache.generate();
 		BitmapFont bfont = FontCache.getFont(FontCache.COURIER_BOLD_20);
 		
 		BitmapString string = new BitmapString(bfont, "AEON");
 		string.setColor(new Color(1.0, 0.0, 0.0));
+		
 		hud.add(string, 400, 300);
-//		string = new BitmapString(bfont, "Corner case");
+		
+		/* Debug box */
+
 		DebugBox box = new DebugBox();
 		box.addObject("Speed", new Integer(200));
 		box.addObject("Something", new Point());
 		hud.add(box, 0, 0);
-		hud.add(new FpsCounter(), Display.getDisplayMode().getWidth() - 100, 0);
-		sr.addBranch(sm);
-	}
-
-	public void keyDown(int key) {
 		
+		/* FPS counter */
+		
+		hud.add(new FpsCounter(), Display.getDisplayMode().getWidth() - 100, 0);
+		
+		/* Simple model */
+		
+		SimpleModel sm = new SimpleModel(new OptimizedGeometry(new SphereGeometry(2.0)));
+		sr.addBranch(sm);
+		
+		/* Model loading*/
+		
+		try {
+			ModelLoader loader = new ModelLoader("data/models/obj/destroyerx/destroyerx.obj");
+			ComplexModel model = loader.load();
+			sr.addBranch(model);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-	public void keyUp(int key) {
-	}
-
+	@Override
 	protected void mainLoop() {
 		sr.activate();
 	}
 
+	@Override
+	public void keyDown(int key) {
+	}
+
+	@Override
+	public void keyUp(int key) {
+	}
+
+	@Override
 	public void mouseDown(Point coords) {
 	}
 
+	@Override
 	public void mouseMove(Point coords) {
 	}
 
+	@Override
 	public void mouseUp(Point coords) {
 	}
 }
