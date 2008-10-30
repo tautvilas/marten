72ecb005825bb1313659f4eb9eb8d86568759d49
carney.fex <carney.fex@673c011e-8dad-11dd-8ab4-67102e0efeef>
2008-10-30 18:52:19 +0000

    Forgot to select all of the updates :-/

---
+++ trunk/aeon/src/main/java/marten/physics/FeatureListener.java
@@ -0,0 +1,5 @@
+package marten.physics;
+
+public interface FeatureListener {
+	void featureChange (Feature feature, String parameter);
+}
---
+++ trunk/aeon/src/main/java/marten/physics/features/ChangeableMass.java
@@ -0,0 +1,33 @@
+package marten.physics.features;
+
+import marten.physics.PhysicsObject;
+
+public class ChangeableMass extends Mass {
+	public ChangeableMass(PhysicsObject owner, double newMass) {
+		super(owner, newMass);
+	}
+	@Override public String getSubtype () {
+		return "CHANGEABLE";
+	}
+	@Override public void set (String param, Object value) {
+		if (param.equalsIgnoreCase("MASS")) {
+			if (!(value instanceof Double))
+				throw new RuntimeException ("Attempted to set mass with an object of incompatible type.");
+			if (((Double)value).doubleValue() < 0)
+				throw new RuntimeException ("Attempted to set negative mass.");
+			this.mass = ((Double)value).doubleValue();
+			this.notifyListeners("MASS");
+		}
+		else
+			throw new RuntimeException ("Attempted to set unknown parameter.");
+	}
+	@Override public void setDouble (String param, double value) {
+		if (param.equalsIgnoreCase("MASS")) {
+			if ((this.mass = value) < 0)
+				throw new RuntimeException ("Attempted to set negative mass.");
+			this.notifyListeners("MASS");
+		}
+		else
+			throw new RuntimeException ("Attempted to set unknown parameter.");
+	}
+}
