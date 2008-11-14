69425485a3063fbbcdb84c3f643d310317fbf8f4
carney.fex <carney.fex@673c011e-8dad-11dd-8ab4-67102e0efeef>
2008-11-14 12:22:28 +0000

    Three new classes to support various types of movement and positioning: * Zero velocity, constant position. * Constant velocity, changing position. * Changing velocity, changing position.

---
+++ trunk/aeon/src/main/java/marten/physics/features/Position.java
@@ -0,0 +1,60 @@
+package marten.physics.features;
+
+import marten.physics.Feature;
+import marten.physics.PhysicsObject;
+import marten.util.Point;
+import marten.util.Rotation;
+import marten.util.Vector;
+
+public class Position extends Feature {
+	protected Point position;
+	public Position (PhysicsObject owner, Point newPosition) {
+		super (owner);
+		this.position = newPosition;
+	}
+	@Override public void execute(double time) {}
+	@Override public Object get(String param) {
+		if (param.equalsIgnoreCase("POSITION"))
+			return this.position;
+		if (param.equalsIgnoreCase("VELOCITY"))
+			return new Vector ();
+		throw new RuntimeException ("Unsupported parameter " + param + " requested.");
+	}
+	@Override public double getDouble(String param) {
+		throw new RuntimeException ("Unsupported parameter " + param + " requested.");
+	}
+	@Override public Point getPoint(String param) {
+		if (param.equalsIgnoreCase("POSITION"))
+			return this.position;
+		throw new RuntimeException ("Unsupported parameter " + param + " requested.");
+	}
+	@Override public Rotation getRotation(String param) {
+		throw new RuntimeException ("Unsupported parameter " + param + " requested.");
+	}
+	@Override public String getSubtype() {
+		return "STATIONARY";
+	}
+	@Override public String getType() {
+		return "POSITION";
+	}
+	@Override public Vector getVector(String param) {
+		if (param.equalsIgnoreCase("VELOCITY"))
+			return new Vector();
+		throw new RuntimeException ("Unsupported parameter " + param + " requested.");
+	}
+	@Override public void set(String param, Object value) {
+		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
+	}
+	@Override public void setDouble(String param, double value) {
+		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
+	}
+	@Override public void setPoint(String param, Point value) {
+		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
+	}
+	@Override public void setRotation(String param, Rotation value) {
+		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
+	}
+	@Override public void setVector(String param, Vector value) {
+		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
+	}
+}
