1c96a5cec3d1e320d99cd996a8053669f3f71d54
carney.fex <carney.fex@673c011e-8dad-11dd-8ab4-67102e0efeef>
2008-11-23 11:13:43 +0000

    Minor fuck-up.

--- trunk/aeon/src/main/java/marten/physics/interactions/Gravity.java
+++ trunk/aeon/src/main/java/marten/physics/interactions/Gravity.java
@@ -3,8 +3,6 @@ package marten.physics.interactions;
 import marten.physics.Feature;
 import marten.physics.FeatureListener;
 import marten.physics.PhysicsObject;
-import marten.util.Rotation;
-import marten.util.Vector;
 
 public class Gravity extends DistanceSquaredAttractor implements FeatureListener {
 	public static final double GRAVITY_CONSTANT = 6.674E-11;
@@ -17,10 +15,9 @@ public class Gravity extends DistanceSquaredAttractor implements FeatureListener
 	public void featureChange(Feature feature, String parameter, double value) {
 		if (parameter.equalsIgnoreCase("MASS"))
 			this.gravitationalParameter = value * Gravity.GRAVITY_CONSTANT;
+		else
+			super.featureChange(feature, parameter, value);
 	}
-	public void featureChange(Feature feature, String parameter, Vector value) {}
-	public void featureChange(Feature feature, String parameter, Rotation value) {}
-	public void featureChange(Feature feature, String parameter, Object value) {}
 	@Override protected double attractiveForce() {
 		return this.gravitationalParameter;
 	}
