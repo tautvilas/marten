2c9f6c9db388ad87b0720edf4413cbec9e6f9937
carney.fex <carney.fex@673c011e-8dad-11dd-8ab4-67102e0efeef>
2009-01-06 19:05:01 +0000

    Gotta keep this thing moving, even if very slowly.

--- aeon/src/main/java/marten/physics/interactions/ElectrostaticForce.java
+++ aeon/src/main/java/marten/physics/interactions/ElectrostaticForce.java
@@ -8,18 +8,24 @@ import marten.util.Point;
 import marten.util.Rotation;
 import marten.util.Vector;
 
-public abstract class DistanceSquaredAttractor extends Interaction implements FeatureListener {
+public final class ElectrostaticForce extends Interaction implements FeatureListener {
+	public static final double ELECTRIC_CONSTANT = 8.988E9;
+	private double electricParameter;
 	private Point positionBackup;
-	public DistanceSquaredAttractor (PhysicsObject newOwner, Iterable<PhysicsObject> newAffectedObjects) {
-		super (newOwner, newAffectedObjects);
+	public ElectrostaticForce (PhysicsObject owner, Iterable<PhysicsObject> affectedObjects) {
+		super (owner, affectedObjects);
+		this.getOwner().getFeature("CHARGE").addListener(this);
 		this.getOwner().getFeature("POSITION").addListener(this);
+		this.electricParameter = ElectrostaticForce.ELECTRIC_CONSTANT * owner.getFeature("CHARGE").getDouble("CHARGE");
 		this.positionBackup = this.getOwner().getFeature("POSITION").getPoint("POSITION");
 	}
-	@Override protected final void interact(PhysicsObject target, double time) {
-		Vector direction = new Vector (target.getFeature("POSITION").getPoint("POSITION"), this.positionBackup);
-		target.getFeature("POSITION").setVector("ACCELERATION", direction.normalize().scale(this.attractiveForce () / direction.lengthSquared()));
+	@Override public String getType() {
+		return "ELECTROSTATIC";
 	}
-	protected abstract double attractiveForce ();
+	@Override protected void interact(PhysicsObject target, double time) {
+		Vector direction = new Vector (this.positionBackup, target.getFeature("POSITION").getPoint("POSITION"));
+		target.getFeature("POSITION").setVector("FORCE", direction.normalize().scale(this.electricParameter * target.getFeature("CHARGE").getDouble("CHARGE") / direction.lengthSquared()));
+	}
 	@Override public Object get(String param) {
 		throw new RuntimeException ("Unsupported parameter " + param + " requested.");
 	}
@@ -36,26 +42,28 @@ public abstract class DistanceSquaredAttractor extends Interaction implements Fe
 		throw new RuntimeException ("Unsupported parameter " + param + " requested.");
 	}
 	@Override public void set(String param, Object value) {
-		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
+		throw new RuntimeException ("Unsupported parameter " + param + " requested.");
 	}
 	@Override public void setDouble(String param, double value) {
-		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
+		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");		
 	}
 	@Override public void setPoint(String param, Point value) {
-		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
+		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");		
 	}
 	@Override public void setRotation(String param, Rotation value) {
 		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
 	}
 	@Override public void setVector(String param, Vector value) {
-		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");
+		throw new RuntimeException ("Attempted to set unknown parameter " + param + ".");		
 	}
-	public void featureChange(Feature feature, String parameter, double value) {}
+	public void featureChange (Feature feature, String parameter, double value) {
+		if (parameter.equalsIgnoreCase("CHARGE"))
+			this.electricParameter = ElectrostaticForce.ELECTRIC_CONSTANT * value;
+	}
 	public void featureChange(Feature feature, String parameter, Vector value) {}
 	public void featureChange(Feature feature, String parameter, Point value) {
 		if (parameter.equalsIgnoreCase("POSITION"))
 			this.positionBackup = value;
-		
 	}
 	public void featureChange(Feature feature, String parameter, Rotation value) {}
 	public void featureChange(Feature feature, String parameter, Object value) {}
