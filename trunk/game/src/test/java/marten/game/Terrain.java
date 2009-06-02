daecae7037bceedc2356cff167cf2686f41629cb
carney.fex <carney.fex@673c011e-8dad-11dd-8ab4-67102e0efeef>
2009-06-02 21:28:51 +0000

    Is it working yet?

---
+++ trunk/game/src/test/java/marten/game/Terrain.java
@@ -0,0 +1,14 @@
+package marten.game;
+
+/** Defines the different generic types of terrain, recognized by the game.
+ * @author carnifex*/
+public enum Terrain {
+	PLAIN /*Grass*/,
+	DIFFICULT /*Marsh*/,
+	OBSTRUCTED /*Forest*/,
+	ELEVATED /*Hill*/,
+	MOUNTAINOUS /*Mountains*/,
+	FORTIFIED /*Trenches*/,
+	LIQUID /*Water*/,
+	VOID /*Chasm*/
+}
