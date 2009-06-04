d10fca42aec7f424ba8265ae7409cf356dedcf04
carney.fex <carney.fex@673c011e-8dad-11dd-8ab4-67102e0efeef>
2009-06-04 18:03:43 +0000

    Optimized MapCoordinates.adjacent(MapDirection) method.

--- trunk/game/src/main/java/marten/game/MapCoordinates.java
+++ trunk/game/src/main/java/marten/game/MapCoordinates.java
@@ -1,42 +1,45 @@
 package marten.game;
 
+/**This class should be everywhere a location on the map is to be referenced.
+ * @author carnifex*/
 public class MapCoordinates {
+    /**The x coordinate (from bottom up)*/
     public int x;
+    /**The y coordinate (from left to right)*/
     public int y;
+    /**A constructor with initial coordinate values.
+     * @param x the initial x coordinate
+     * @param y the initial y coordinate*/
     public MapCoordinates (int x, int y) {
         this.x = x;
         this.y = y;
     }
+    /**This method evaluates the coordinates of a location, adjacent to these coordinates in the given direction.
+     * @param direction the direction to which the adjacent location is found
+     * @return the coordinates of the adjacent location*/
     public MapCoordinates adjacent (MapDirection direction) {
+        int newX = this.x;
+        int newY = this.y;
+        switch (direction) {
+        case NORTHWEST:
+        case SOUTHWEST:
+            --newX; break;
+        case NORTHEAST:
+        case SOUTHEAST:
+            ++newX; break;
+        }
         switch (direction) {
         case NORTH:
-            return new MapCoordinates(x, y + 1);
+            ++newY; break;
+        case NORTHWEST:
+        case NORTHEAST:
+            newY += (this.x + 1) % 2; break;
+        case SOUTHWEST:
+        case SOUTHEAST:
+            newY -= this.x % 2; break;
         case SOUTH:
-            return new MapCoordinates(x, y - 1);
+            --newY; break;
         }
-        if (x % 2 == 0)
-            switch (direction) {
-            case NORTHWEST:
-                return new MapCoordinates(x - 1, y + 1);
-            case NORTHEAST:
-                return new MapCoordinates(x + 1, y + 1);
-            case SOUTHWEST:
-                return new MapCoordinates(x - 1, y);
-            case SOUTHEAST:
-                return new MapCoordinates(x + 1, y);
-            }                
-        else
-            switch (direction) {
-            case NORTHWEST:
-                return new MapCoordinates(x - 1, y);
-            case NORTHEAST:
-                return new MapCoordinates(x + 1, y);
-            case SOUTHWEST:
-                return new MapCoordinates(x - 1, y - 1);
-            case SOUTHEAST:
-                return new MapCoordinates(x + 1, y - 1);
-            }
-        /* This should never be reached */
-        throw new RuntimeException ("Behold a miracle. Someone messed with MapDirection class apparently.");
+        return new MapCoordinates(newX, newY);
     }
 }
