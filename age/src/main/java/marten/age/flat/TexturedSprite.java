3915faccb9b54c789fb82d6b5358f78139ceed1d
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2009-07-05 21:00:56 +0000

    Solved a small problem with TexturedSprite
    We will not be using this class though, it was just
    professional curiosity.

--- age/src/main/java/marten/age/flat/TexturedSprite.java
+++ age/src/main/java/marten/age/flat/TexturedSprite.java
@@ -5,20 +5,22 @@ import marten.age.geometry.SimpleModel;
 import marten.age.geometry.primitives.Rectangle;
 import marten.age.texture.Texture;
 import marten.age.transform.TranslationGroup;
+import marten.age.util.Color;
 import marten.util.Point;
 
-public class Sprite3D extends BasicSceneGraphBranch {
+public class TexturedSprite extends BasicSceneGraphBranch {
     private SimpleModel model;
     private TranslationGroup tg = new TranslationGroup();
 
-    public Sprite3D(Texture texture) {
+    public TexturedSprite(Texture texture) {
         model = new SimpleModel(new Rectangle(texture.getDimension()));
+        model.getAppearance().setColor(new Color(1.0, 0.5, 0.0));
         model.getAppearance().setTexture(texture);
         tg.addChild(model);
         this.addChild(tg);
     }
 
-    public Sprite3D(Texture texture, Point position) {
+    public TexturedSprite(Texture texture, Point position) {
         this(texture);
         setPosition(position);
     }
