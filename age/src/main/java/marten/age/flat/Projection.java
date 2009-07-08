747380b8cae4701cd88c07b15802d230e1d8a3fa
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2009-07-08 20:38:03 +0000

    moved marten.age.hud to marten.age.flat

--- age/src/main/java/marten/age/flat/Projection.java
+++ age/src/main/java/marten/age/flat/Projection.java
@@ -1,4 +1,4 @@
-package marten.age.hud;
+package marten.age.flat;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -9,18 +9,16 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.glu.GLU;
 
 public class Projection {
-	public static Point unproject(Point windowCoords) {
-		float[] position = new float[3];
-		GLU.gluUnProject((float)windowCoords.x,
-				(float)windowCoords.y,
-				(float)windowCoords.z,
-				getModelviewMatrix(), getProjectionMatrix(), getViewport(), position);
-		return new Point(position[0], position[1], position[2]);
-	}
-    
-    private static float[][] getModelviewMatrix()
-    {
-    	float[][] modelViewMatrix = new float[4][4];
+    public static Point unproject(Point windowCoords) {
+        float[] position = new float[3];
+        GLU.gluUnProject((float) windowCoords.x, (float) windowCoords.y,
+                (float) windowCoords.z, getModelviewMatrix(),
+                getProjectionMatrix(), getViewport(), position);
+        return new Point(position[0], position[1], position[2]);
+    }
+
+    private static float[][] getModelviewMatrix() {
+        float[][] modelViewMatrix = new float[4][4];
         FloatBuffer bufferModelviewMatrix = FloatBuffer.allocate(16);
         bufferModelviewMatrix.clear();
         GL11.glGetFloat(GL11.GL_MODELVIEW_MATRIX, bufferModelviewMatrix);
@@ -28,9 +26,8 @@ public class Projection {
         return modelViewMatrix;
     }
 
-    private static float[][] getProjectionMatrix()
-    {
-    	float[][] projectionMatrix = new float[4][4];
+    private static float[][] getProjectionMatrix() {
+        float[][] projectionMatrix = new float[4][4];
         FloatBuffer bufferProjectionMatrix = FloatBuffer.allocate(16);
         bufferProjectionMatrix.clear();
         GL11.glGetFloat(GL11.GL_PROJECTION_MATRIX, bufferProjectionMatrix);
@@ -38,19 +35,18 @@ public class Projection {
         return projectionMatrix;
     }
 
-    private static int[] getViewport()
-    {
+    private static int[] getViewport() {
         IntBuffer bufferViewport = IntBuffer.allocate(16);
         bufferViewport.clear();
         GL11.glGetInteger(GL11.GL_VIEWPORT, bufferViewport);
         return bufferViewport.array();
     }
-    
-    private static void bufferToArray(FloatBuffer fb , float[][] fa) {
-    	for (int i = 0; i < fa.length; i++) {
-    		for (int j = 0; j < fa[i].length; j++) {
-    			fa[i][j] = fb.get();
-    		}
-    	}
+
+    private static void bufferToArray(FloatBuffer fb, float[][] fa) {
+        for (int i = 0; i < fa.length; i++) {
+            for (int j = 0; j < fa[i].length; j++) {
+                fa[i][j] = fb.get();
+            }
+        }
     }
 }
