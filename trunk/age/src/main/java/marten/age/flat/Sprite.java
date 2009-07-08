7db455b15751afb37f382475160e7ea3ff601e28
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2009-07-08 20:34:47 +0000

    Minor age novelty: Image package

--- trunk/age/src/main/java/marten/age/flat/Sprite.java
+++ trunk/age/src/main/java/marten/age/flat/Sprite.java
@@ -1,60 +1,17 @@
 package marten.age.flat;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.io.File;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-import javax.imageio.ImageIO;
-
 import marten.age.BasicSceneGraphChild;
-import marten.age.util.Constants;
-import marten.util.Dimension;
+import marten.age.image.ImageData;
 import marten.util.Point;
 
-import org.apache.log4j.Logger;
 import org.lwjgl.opengl.GL11;
 
 public class Sprite extends BasicSceneGraphChild{
-    private static org.apache.log4j.Logger log =
-            Logger.getLogger(Sprite.class);
-
     private Point position = new Point();
-    private Dimension dimension = new Dimension();
+    private ImageData data;
 
-    ByteBuffer byteBuffer = null;
-    int format;
-
-    public Sprite(String fileName) {
-        BufferedImage image;
-        try {
-            image = ImageIO.read(new File(fileName));
-        } catch (IOException e) {
-            throw new RuntimeException("Image load fail");
-        }
-        byte[] data = ((DataBufferByte)(image).getRaster().getDataBuffer())
-                .getData();
-        byteBuffer = ByteBuffer.wrap(data);
-        byteBuffer.rewind();
-        dimension.width = image.getWidth();
-        dimension.height = image.getHeight();
-        log.info("Sprite initialized: H: " +
-                dimension.height + " W:" + dimension.width + " B:" +
-                byteBuffer);
-        
-        if (data.length == dimension.width *
-                dimension.height * Constants.RGB_NUM_BYTES) {
-            format = GL11.GL_RGB;
-            log.info("RGB");
-        } else if (data.length == dimension.width * dimension.height
-                * Constants.RGBA_NUM_BYTES) {
-            format = GL11.GL_RGBA;
-            log.info("RGBA");
-        } else {
-            throw new RuntimeException("Image data mismatch.");
-        }
-
+    public Sprite(ImageData data) {
+        this.data = data;
     }
 
     public void setPosition(Point point) {
@@ -64,7 +21,7 @@ public class Sprite extends BasicSceneGraphChild{
     @Override
     public void activate() {
         GL11.glRasterPos2d(position.x, position.y);
-        GL11.glDrawPixels((int)dimension.width, (int)dimension.height,
-                format, GL11.GL_UNSIGNED_BYTE, byteBuffer);
+        GL11.glDrawPixels((int)data.width, (int)data.height,
+                data.getType(), GL11.GL_UNSIGNED_BYTE, data.getByteBuffer());
     }
 }
