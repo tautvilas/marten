ffed5e6967bdfcb2906e080c9664a49d7dea0531
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2009-08-30 12:01:03 +0000

    This goes with last commit.

--- trunk/marten-testing/src/main/java/marten/testing/tests/loadingtest/FirstScene.java
+++ trunk/marten-testing/src/main/java/marten/testing/tests/loadingtest/FirstScene.java
@@ -1,12 +1,11 @@
 package marten.testing.tests.loadingtest;
 
-import java.io.IOException;
-
 import marten.age.core.AgeScene;
 import marten.age.event.AgeEvent;
 import marten.age.flat.Flatland;
 import marten.age.flat.Sprite;
 import marten.age.image.ImageData;
+import marten.age.image.ImageLoader;
 import marten.util.Point;
 
 public class FirstScene implements AgeScene {
@@ -16,16 +15,10 @@ public class FirstScene implements AgeScene {
     @Override
     public void init() {
         // TODO:zv:develop a decent image loading/caching system
-        ImageData buttonImage;
-        try {
-            buttonImage = new ImageData("data/testloading/button.png");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        ImageData buttonImage = ImageLoader.loadImage("data/testloading/button.png");
         flatland = new Flatland();
         Sprite button = new Sprite(buttonImage);
-        flatland.addSprite(button, new Point(100, 100));
+        flatland.addSprite(button, new Point(200, 200));
     }
 
     @Override
