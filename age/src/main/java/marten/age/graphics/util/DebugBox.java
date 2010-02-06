a6eb4b678db2577f6b06b17fc014a95629bb12ef
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-02-06 18:00:41 +0000

    Font cache revamp.

--- age/src/main/java/marten/age/graphics/util/DebugBox.java
+++ age/src/main/java/marten/age/graphics/util/DebugBox.java
@@ -1,5 +1,6 @@
 package marten.age.graphics.util;
 
+import java.awt.Font;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -10,32 +11,32 @@ import marten.age.graphics.text.FontCache;
 
 import org.lwjgl.opengl.GL11;
 
-
 public class DebugBox extends BasicSceneGraphChild {
-	
-	private HashMap<String, Object> objects = new HashMap<String, Object>();
 
-	private static final long serialVersionUID = 1L;
-	
-	private BitmapFont font = FontCache.getFont(FontCache.COURIER_14);
-	
-	public void addObject(String key, Object o) {
-		objects.put(key, o);
-	}
-	
-	public void removeObject(String key) {
-		objects.remove(key);
-	}
-	
-	public void render() {
-		GL11.glPushMatrix();
-		Set<String> keys = objects.keySet();
-		for (String key : keys) {
-			Object o = objects.get(key);
-			BitmapString string = new BitmapString(font, key + ": " + o.toString());
-			string.render();
-			GL11.glTranslated(0.0, font.getSize(), 0.0);
-		}
-		GL11.glPopMatrix();
-	}
+    private HashMap<String, Object> objects = new HashMap<String, Object>();
+
+    private static final long serialVersionUID = 1L;
+
+    BitmapFont font = FontCache.getFont(new Font("Courier New", Font.BOLD, 14));
+
+    public void addObject(String key, Object o) {
+        objects.put(key, o);
+    }
+
+    public void removeObject(String key) {
+        objects.remove(key);
+    }
+
+    public void render() {
+        GL11.glPushMatrix();
+        Set<String> keys = objects.keySet();
+        for (String key : keys) {
+            Object o = objects.get(key);
+            BitmapString string = new BitmapString(font, key + ": "
+                    + o.toString());
+            string.render();
+            GL11.glTranslated(0.0, font.getSize(), 0.0);
+        }
+        GL11.glPopMatrix();
+    }
 }
