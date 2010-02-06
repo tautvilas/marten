a4fb8c0f22ae7955a05fac01cf640089e3478019
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-02-06 14:16:27 +0000

    Remove unnecessary projects.

--- cage/src/main/java/marten/cage/Collidable.java
+++
@@ -1,5 +0,0 @@
-package marten.cage;
-
-public interface Collidable {
-    public CollisionModel getCollisionModel();
-}
--- game/src/main/java/marten/game/map/MapListener.java
+++
@@ -1,5 +0,0 @@
-package marten.game.map;
-
-public interface MapListener {
-    void onTerrainChange(Map map, MapCoordinates at, Terrain oldTerrain, Terrain newTerrain);
-}
