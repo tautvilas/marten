2984331e3df18b6ad13580513d0f340571a13bfa
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-03-05 16:33:54 +0000

    Dealing with different image data formats.

--- age/src/main/java/marten/age/graphics/image/ImageData.java
+++ age/src/main/java/marten/age/graphics/image/ImageData.java
@@ -1,7 +1,6 @@
 package marten.age.graphics.image;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -12,11 +11,9 @@ import marten.age.graphics.util.Constants;
 
 import org.lwjgl.opengl.GL11;
 
-
 public class ImageData {
     private byte[] buffer;
     private ByteBuffer byteBuffer;
-    public int type;
     public int height;
     public int width;
 
@@ -29,13 +26,62 @@ public class ImageData {
     }
 
     public ImageData(BufferedImage image) {
-        this(((DataBufferByte) (image).getRaster().getDataBuffer()).getData(),
-                image.getWidth(), image.getHeight());
+        int[] pixels = image.getRGB(0, 0, image.getWidth(), image.getHeight(),
+                null, 0, image.getWidth());
+        int type = image.getType();
+        byte[] data = new byte[pixels.length << 2];
+        for (int i = 0; i < pixels.length; i++) {
+            int x = pixels[i];
+            int j = i << 2;
+            if (type == BufferedImage.TYPE_4BYTE_ABGR) {
+                data[j + 2] = (byte)(x & 0xff);
+                data[j + 1] = (byte)((x >>> 8) & 0xff);
+                data[j + 0] = (byte)((x >>> 16) & 0xff);
+                data[j + 3] = (byte)((x >>> 24) & 0xff);
+            } else if (type == BufferedImage.TYPE_3BYTE_BGR) {
+                data[j + 2] = (byte)(x & 0xff);
+                data[j + 1] = (byte)((x >>> 8) & 0xff);
+                data[j + 0] = (byte)((x >>> 16) & 0xff);
+                data[j + 3] = (byte)0xff;
+            } else if (type == BufferedImage.TYPE_INT_ARGB) {
+                data[j + 0] = (byte)(x & 0xff);
+                data[j + 1] = (byte)((x >>> 8) & 0xff);
+                data[j + 2] = (byte)((x >>> 16) & 0xff);
+                data[j + 3] = (byte)((x >>> 24) & 0xff);
+            } else if (type == BufferedImage.TYPE_INT_BGR) {
+                data[j + 2] = (byte)(x & 0xff);
+                data[j + 1] = (byte)((x >>> 8) & 0xff);
+                data[j + 0] = (byte)((x >>> 16) & 0xff);
+                data[j + 3] = (byte)0xff;
+            } else if (type == BufferedImage.TYPE_INT_RGB) {
+                data[j + 0] = (byte)(x & 0xff);
+                data[j + 1] = (byte)((x >>> 8) & 0xff);
+                data[j + 2] = (byte)((x >>> 16) & 0xff);
+                data[j + 3] = (byte)0xff;
+            } else {
+                throw new RuntimeException("Unsupported BufferedImage type: " + type);
+            }
+            j += 4;
+        }
+        this.width = image.getWidth();
+        this.height = image.getHeight();
+        this.buffer = data;
+        byteBuffer = ByteBuffer.wrap(buffer);
+        byteBuffer.rewind();
+        sanityCheck();
     }
 
     public ImageData(byte[] buffer, int width, int height) {
-        int test = 0;
+        this.width = width;
+        this.height = height;
+        this.buffer = buffer;
+        byteBuffer = ByteBuffer.wrap(buffer);
+        byteBuffer.rewind();
+        sanityCheck();
+    }
 
+    private void sanityCheck() {
+        int test = 0;
         while (width >> test != 1)
             test++;
         if (width >> test << test != width)
@@ -43,29 +89,21 @@ public class ImageData {
         test = 0;
         while (height >> test != 1)
             test++;
+
         if (height >> test << test != height)
             throw new RuntimeException("Image height is not a power of two.");
 
-        if (buffer.length == width * height * Constants.RGB_NUM_BYTES) {
-            this.type = GL11.GL_RGB;
-        } else if (buffer.length == width * height * Constants.RGBA_NUM_BYTES) {
-            this.type = GL11.GL_RGBA;
-        } else {
+        if (buffer.length != width * height * Constants.RGBA_NUM_BYTES) {
             throw new RuntimeException("Image data mismatch.");
         }
-        this.width = width;
-        this.height = height;
-        this.buffer = buffer;
-        byteBuffer = ByteBuffer.wrap(buffer);
-        byteBuffer.rewind();
     }
 
     public byte[] getBuffer() {
         return this.buffer;
     }
 
-    public int getType() {
-        return this.type;
+    public int getPixelType() {
+        return GL11.GL_RGBA;
     }
 
     public ByteBuffer getByteBuffer() {
