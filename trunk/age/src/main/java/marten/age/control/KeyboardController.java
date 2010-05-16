314b37452f6c929bf60b9d47df780390a759245e
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-05-16 13:16:22 +0000

    Moved controller framework to AgeScene.

--- trunk/age/src/main/java/marten/age/control/KeyboardController.java
+++ trunk/age/src/main/java/marten/age/control/KeyboardController.java
@@ -4,31 +4,37 @@ import org.lwjgl.input.Keyboard;
 
 public class KeyboardController extends Controller {
 
-	public KeyboardController() {
-	}
-	
-	public KeyboardController(KeyboardListener l) {
-		super.addListener(l);
-	}
-	
-	public void addListener(KeyboardListener l) {
-		super.addListener(l);
-	}
+    public KeyboardController() {
+    }
 
-	@Override
-	protected void publishEventsToListener(Listener l) {
-		KeyboardListener listener = (KeyboardListener)l;
-		while ( Keyboard.next() )  {
-			if (Keyboard.getEventKeyState()) {
-				listener.keyDown(Keyboard.getEventKey());
-			}
-			else {
-				listener.keyUp(Keyboard.getEventKey());
-			}
-		}
-	}
+    public KeyboardController(KeyboardListener l) {
+        super.addListener(l);
+    }
 
-	@Override
-	protected void harvestEvents() {
-	}
+    public void addListener(KeyboardListener l) {
+        super.addListener(l);
+    }
+
+    @Override
+    protected void publishEventsToListener(Listener l) {
+        KeyboardListener listener = (KeyboardListener) l;
+        while (Keyboard.next()) {
+            if (Keyboard.getEventKeyState()) {
+                listener.keyDown(Keyboard.getEventKey());
+            } else {
+                listener.keyUp(Keyboard.getEventKey());
+            }
+        }
+    }
+
+    @Override
+    protected void harvestEvents() {
+    }
+
+    @Override
+    protected void listenerTypeCheck(Listener l) {
+        if (!(l instanceof KeyboardListener)) {
+            throw new ClassCastException("Incorrect listener type");
+        }
+    }
 }
