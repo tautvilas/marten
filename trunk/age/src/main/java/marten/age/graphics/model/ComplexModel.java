0c972f43e1fc77db9e3198a88b61d6de1aedb061
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-08-02 17:05:49 +0000

    Simple model is capable of storing several geometries.

--- trunk/age/src/main/java/marten/age/graphics/model/ComplexModel.java
+++ trunk/age/src/main/java/marten/age/graphics/model/ComplexModel.java
@@ -5,21 +5,26 @@ import java.util.ArrayList;
 import marten.age.graphics.BasicSceneGraphChild;
 
 public class ComplexModel extends BasicSceneGraphChild {
-	private ArrayList<SimpleModel> parts = new ArrayList<SimpleModel>();
-	public void addPart (SimpleModel newPart) {
-		parts.add (newPart);
-	}
-	public void removePart (SimpleModel oldPart) {
-		parts.remove(oldPart);
-	}
-	public SimpleModel getFirstPart() {
-		return parts.get(0);
-	}
-	public ArrayList<SimpleModel> getParts() {
-		return parts;
-	}
-	public void render() {
-		for (SimpleModel part : parts)
-			part.render();
-	}	
+    private ArrayList<SimpleModel> parts = new ArrayList<SimpleModel>();
+
+    public void addPart(SimpleModel newPart) {
+        parts.add(newPart);
+    }
+
+    public void removePart(SimpleModel oldPart) {
+        parts.remove(oldPart);
+    }
+
+    public SimpleModel getFirstPart() {
+        return parts.get(0);
+    }
+
+    public ArrayList<SimpleModel> getParts() {
+        return parts;
+    }
+
+    public void render() {
+        for (SimpleModel part : parts)
+            part.render();
+    }
 }
--- trunk/age/src/main/java/marten/age/graphics/model/SimpleModel.java
+++ trunk/age/src/main/java/marten/age/graphics/model/SimpleModel.java
@@ -1,35 +1,51 @@
 package marten.age.graphics.model;
 
+import java.util.ArrayList;
+
 import marten.age.graphics.BasicSceneGraphChild;
 import marten.age.graphics.appearance.Appearance;
 import marten.age.graphics.geometry.Geometry;
 
 public class SimpleModel extends BasicSceneGraphChild {
     private Appearance appearance = new Appearance();
-    private Geometry geometry = null;
+    private ArrayList<Geometry> geometries = new ArrayList<Geometry>();
 
     public SimpleModel(Geometry geometry) {
-        this.geometry = geometry;
+        this.geometries.add(geometry);
     }
 
     public void setAppearance(Appearance appearance) {
         this.appearance = appearance;
     }
 
-    public void setGeometry(Geometry newGeometry) {
-        geometry = newGeometry;
+    public void addGeometry(Geometry newGeometry) {
+        this.geometries.add(newGeometry);
     }
 
     public Appearance getAppearance() {
         return appearance;
     }
 
-    public Geometry getGeometry() {
-        return geometry;
+    public ArrayList<Geometry> getGeometries() {
+        return this.geometries;
+    }
+
+    public void setGeometries(ArrayList<Geometry> geometries) {
+        this.geometries = geometries;
+    }
+ 
+    public Geometry getFirstGeometry() {
+        if (this.geometries.size() == 0) {
+            return null;
+        } else {
+            return this.geometries.get(0);
+        }
     }
 
     public void render() {
         appearance.set();
-        geometry.draw();
+        for (Geometry geometry : this.geometries) {
+            geometry.draw();
+        }
     }
 }
