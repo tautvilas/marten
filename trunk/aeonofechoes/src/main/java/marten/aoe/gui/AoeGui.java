de585d4298001ab1da566c0df49b97a5f5d626c9
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-09-10 13:49:29 +0000

    Main menu for AOE.

--- trunk/aeonofechoes/src/main/java/marten/aoe/gui/AoeGui.java
+++ trunk/aeonofechoes/src/main/java/marten/aoe/gui/AoeGui.java
@@ -1,14 +1,13 @@
 package marten.aoe.gui;
 
 import marten.age.core.AgeApp;
-import marten.aoe.gui.scene.Game;
+import marten.aoe.gui.scene.MainMenu;
 
 public class AoeGui extends AgeApp {
 
     @Override
     public void configure() {
-//        this.setActiveScene(new MainMenu());
-        this.setActiveScene(new Game("mapTest"));
+        this.setActiveScene(new MainMenu());
     }
 
 }
