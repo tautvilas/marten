4802c390eeac21efade593c6c261bd11b26eed6c
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-09-12 20:25:00 +0000

    Identation.

--- age/src/main/java/marten/age/graphics/BasicSceneGraphBranch.java
+++ age/src/main/java/marten/age/graphics/BasicSceneGraphBranch.java
@@ -1,15 +1,16 @@
 package marten.age.graphics;
 
-public class BasicSceneGraphBranch extends BasicSceneGraphParent implements SceneGraphBranch {
-	private BasicSceneGraphParent root = null;
+public class BasicSceneGraphBranch extends BasicSceneGraphParent implements
+        SceneGraphBranch {
+    private BasicSceneGraphParent root = null;
 
-	@Override
-	public BasicSceneGraphParent getRoot() {
-		return this.root;
-	}
-	
-	@Override
-	public void setRoot(BasicSceneGraphParent newRoot) {
-		this.root = newRoot;
-	}
+    @Override
+    public BasicSceneGraphParent getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public void setRoot(BasicSceneGraphParent newRoot) {
+        this.root = newRoot;
+    }
 }
--- age/src/main/java/marten/age/graphics/SceneGraphChild.java
+++ age/src/main/java/marten/age/graphics/SceneGraphChild.java
@@ -2,6 +2,6 @@ package marten.age.graphics;
 
 public interface SceneGraphChild extends SceneGraphNode {
     public BasicSceneGraphParent getRoot();
-    
+
     public void setRoot(BasicSceneGraphParent newRoot);
 }
