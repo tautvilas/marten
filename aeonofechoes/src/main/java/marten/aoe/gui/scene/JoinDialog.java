55b4110bb77f6997831069c1906a1da87349b081
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-09-12 19:06:05 +0000

    ImageCache.

--- aeonofechoes/src/main/java/marten/aoe/gui/scene/JoinDialog.java
+++ aeonofechoes/src/main/java/marten/aoe/gui/scene/JoinDialog.java
@@ -1,8 +1,26 @@
 package marten.aoe.gui.scene;
 
 import marten.age.core.AgeScene;
+import marten.age.graphics.flat.Flatland;
+import marten.age.graphics.flat.sprite.TextureSprite;
+import marten.age.graphics.image.ImageCache;
+import marten.age.graphics.image.ImageData;
+import marten.aoe.Path;
+
+import org.apache.log4j.Logger;
 
 public class JoinDialog extends AgeScene {
+
+    @SuppressWarnings("unused")
+    private static org.apache.log4j.Logger log = Logger.getLogger(JoinDialog.class);
+    private Flatland flatland = new Flatland();
+
+    public JoinDialog() {
+        // load graphics
+        ImageData menuButtonImage = ImageCache.getImage(Path.SKIN_PATH + "menu-button.png");
+        this.flatland.addChild(new TextureSprite(menuButtonImage));
+        this.flatland.compile();
+    }
 
     @Override
     public void compute() {
@@ -10,6 +28,7 @@ public class JoinDialog extends AgeScene {
 
     @Override
     public void render() {
+        flatland.render();
     }
 
 }
---
+++ age/src/main/java/marten/age/graphics/image/ImageCache.java
@@ -0,0 +1,29 @@
+package marten.age.graphics.image;
+
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+
+public class ImageCache {
+    private static org.apache.log4j.Logger log = Logger.getLogger(ImageCache.class);
+    private static HashMap<String, ImageData> images = new HashMap<String, ImageData>();
+
+    public static void loadImage(String path) {
+        if (!images.containsKey(path)) {
+            ImageData image = ImageLoader.loadImage(path);
+            images.put(path, image);
+            log.info("Image " + path + " added to image cache.");
+        } else {
+            log.info("Image " + path + " is allready in font cache.");
+        }
+    }
+
+    public static ImageData getImage(String path) {
+        if (!images.containsKey(path)) {
+            loadImage(path);
+        } else {
+            log.info("Found image " + path + " in image cache. Returning.");
+        }
+        return images.get(path);
+    }
+}
