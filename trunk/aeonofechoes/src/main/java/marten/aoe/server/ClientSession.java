e0796415c83fc1d963cece78ea028be2bf622da3
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-10-01 10:59:23 +0000

    Client session should be serializable

--- trunk/aeonofechoes/src/main/java/marten/aoe/server/ClientSession.java
+++ trunk/aeonofechoes/src/main/java/marten/aoe/server/ClientSession.java
@@ -1,8 +1,11 @@
 package marten.aoe.server;
 
+import java.io.Serializable;
 
-public class ClientSession {
+public class ClientSession implements Serializable {
+    private static final long serialVersionUID = 1L;
     public String username;
+
     public ClientSession(String username) {
         this.username = username;
     }
