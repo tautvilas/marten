57c0a92669e4f63d53287e4755ef1f27edc8d2d0
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-10-08 20:48:04 +0000

    Created "choose map" dialog.

---
+++ aeonofechoes/src/main/java/marten/aoe/gui/scene/menu/ChooseMap.java
@@ -0,0 +1,72 @@
+package marten.aoe.gui.scene.menu;
+
+import java.io.File;
+
+import marten.age.control.MouseController;
+import marten.age.core.AgeScene;
+import marten.age.core.AppInfo;
+import marten.age.event.AgeSceneSwitchEvent;
+import marten.age.graphics.flat.Flatland;
+import marten.age.graphics.flat.SimpleLayout;
+import marten.age.graphics.image.ImageCache;
+import marten.age.graphics.image.ImageData;
+import marten.age.graphics.primitives.Dimension;
+import marten.age.widget.Action;
+import marten.age.widget.Button;
+import marten.aoe.Path;
+import marten.aoe.gui.widget.OkCancelDialog;
+
+public class ChooseMap extends AgeScene {
+
+    private Flatland flatland = new Flatland();
+
+    public ChooseMap() {
+        this.addController(new MouseController());
+
+        SimpleLayout layout = new SimpleLayout(AppInfo.getDisplayDimension());
+        OkCancelDialog okCancel = new OkCancelDialog(new Dimension(600, 50));
+
+        ImageData menuButtonImage = ImageCache.getImage(Path.SKIN_PATH
+                + "menu-button.png");
+
+        File mapFolder = new File(Path.MAP_PATH);
+        String[] filenames = mapFolder.list();
+        for (int i = 0; i < filenames.length; i++) {
+            final String filename = filenames[i];
+            if (filename.charAt(0) == '.') continue;
+            Button mapButton = new Button(menuButtonImage);
+            mapButton.setAction(new Action() {
+                @Override
+                public void perform() {
+                    fireEvent(new AgeSceneSwitchEvent(new GameGate(filename)));
+                }
+            });
+            mapButton.setLabel(filename);
+            layout.centerHorizontally(mapButton, (int)(layout.getDimension().height
+                    - 100 - i * mapButton.getDimension().height));
+            this.registerControllable(mapButton);
+        }
+
+        okCancel.setCancelAction(new Action() {
+            @Override
+            public void perform() {
+                fireEvent(new AgeSceneSwitchEvent(new MainMenu()));
+            }
+        });
+
+        layout.centerHorizontally(okCancel, 100);
+        this.registerControllable(okCancel);
+        this.flatland.addChild(layout);
+        this.flatland.compile();
+    }
+
+    @Override
+    public void compute() {
+    }
+
+    @Override
+    public void render() {
+        this.flatland.render();
+    }
+
+}
