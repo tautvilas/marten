639e98180619a2f0b1496edc63384894e813280f
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2010-10-24 20:02:20 +0000

    Building game gate.

--- aeonofechoes/src/main/java/marten/aoe/gui/scene/GameGate.java
+++ aeonofechoes/src/main/java/marten/aoe/gui/scene/GameGate.java
@@ -2,10 +2,24 @@ package marten.aoe.gui.scene;
 
 import java.net.InetAddress;
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 
+import marten.age.control.KeyboardController;
+import marten.age.control.MouseController;
 import marten.age.core.AgeScene;
+import marten.age.core.AppInfo;
+import marten.age.event.AgeSceneSwitchEvent;
 import marten.age.graphics.flat.Flatland;
+import marten.age.graphics.flat.SimpleLayout;
+import marten.age.graphics.primitives.Dimension;
+import marten.age.graphics.primitives.Point;
+import marten.age.graphics.transform.TranslationGroup;
+import marten.age.widget.Action;
 import marten.aoe.GameInfo;
+import marten.aoe.gui.scene.menu.MainMenu;
+import marten.aoe.gui.widget.AoeField;
+import marten.aoe.gui.widget.AoeString;
+import marten.aoe.gui.widget.OkCancelDialog;
 import marten.aoe.server.AoeServer;
 import marten.aoe.server.ClientSession;
 import marten.aoe.server.face.Server;
@@ -17,8 +31,44 @@ public class GameGate extends AgeScene {
     private String serverUrl;
     private ClientSession session;
     private Flatland flatland = new Flatland();
+    private TranslationGroup players = new TranslationGroup();
+
+    private GameGate() {
+        Dimension dApp = AppInfo.getDisplayDimension();
+        AoeField chatField = new AoeField();
+        OkCancelDialog dialog = new OkCancelDialog(new Dimension(600, 200));
+        AoeString waiting = new AoeString("Waiting for players to join...");
+        AoeString player = new AoeString(GameInfo.nickname);
+        player.setColor(new marten.age.graphics.appearance.Color(0, 1.0, 0.0));
+        player.setPosition(new Point(200, 200));
+        players.addChild(player);
+        SimpleLayout layout = new SimpleLayout(dApp);
+        dialog.setPosition(new Point(dApp.width - 600, 0));
+        flatland.addChild(dialog);
+        layout.centerHorizontally(waiting, (int)(dApp.height - 100));
+        flatland.addChild(layout);
+        flatland.addChild(players);
+        dialog.setOkAction(new Action() {
+            @Override
+            public void perform() {
+            }
+        });
+        dialog.setCancelAction(new Action() {
+            @Override
+            public void perform() {
+                // on this event user shall logout/destroy all games/gamegates
+                fireEvent(new AgeSceneSwitchEvent(new MainMenu()));
+            }
+        });
+        this.addController(new MouseController());
+        this.addController(new KeyboardController());
+        this.registerControllable(chatField);
+        this.registerControllable(dialog);
+        this.flatland.compile();
+    }
 
     public GameGate(String mapName) {
+        this();
         AoeServer.start();
         try {
             Server gameServer = connect(InetAddress.getByName("localhost"));
@@ -30,9 +80,11 @@ public class GameGate extends AgeScene {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        this.registerMessenger();
     }
 
     public GameGate(InetAddress server) {
+        this();
         Server gameServer = connect(server);
         this.session = new ClientSession(GameInfo.nickname);
         try {
@@ -43,6 +95,35 @@ public class GameGate extends AgeScene {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        this.registerMessenger();
+    }
+
+    private void registerMessenger() {
+        Thread messageReceiver = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    String[] members;
+                    try {
+                        members = gate.getMembers(session);
+                    } catch (RemoteException e) {
+                        throw new RuntimeException(e);
+                    }
+                    flatland.removeChild(players);
+                    players = new TranslationGroup();
+                    for (int i = 0; i < members.length; i++) {
+                        AoeString player = new AoeString(members[i]);
+                        player
+                                .setColor(new marten.age.graphics.appearance.Color(
+                                        0, 1.0, 0.0));
+                        player.setPosition(new Point(200, 200 + 50 * i));
+                        players.addChild(player);
+                    }
+                    flatland.addChild(players);
+                }
+            }
+        };
+        messageReceiver.start();
     }
 
     private Server connect(InetAddress server) {
@@ -62,7 +143,7 @@ public class GameGate extends AgeScene {
 
     @Override
     public void render() {
-        flatland.render();
+        this.flatland.render();
     }
 
 }
