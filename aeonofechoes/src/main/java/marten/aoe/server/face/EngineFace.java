5fda39c45dbc56ebbaf221fac0920db7619cf54c
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-02-10 00:53:06 +0000

    Several more methods for EngineFace.

--- aeonofechoes/src/main/java/marten/aoe/server/face/EngineFace.java
+++ aeonofechoes/src/main/java/marten/aoe/server/face/EngineFace.java
@@ -4,12 +4,21 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import marten.aoe.dto.MapDTO;
+import marten.aoe.dto.PlayerDTO;
 import marten.aoe.dto.PointDTO;
 
 public interface EngineFace extends Remote {
     public MapDTO getMap() throws RemoteException;
+
     @Deprecated
     public boolean moveUnit(PointDTO from, PointDTO to) throws RemoteException;
+
     @Deprecated
     public boolean createUnit(String name, PointDTO at) throws RemoteException;
+
+    public void addListener() throws RemoteException;
+
+    public void endTurn() throws RemoteException;
+
+    public PlayerDTO getActivePlayer() throws RemoteException;
 }
