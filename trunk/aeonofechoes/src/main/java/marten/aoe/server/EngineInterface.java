120549bf75d0a4f13e559f500336c097e94a4228
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-02-10 00:53:06 +0000

    Several more methods for EngineFace.

--- trunk/aeonofechoes/src/main/java/marten/aoe/server/EngineInterface.java
+++ trunk/aeonofechoes/src/main/java/marten/aoe/server/EngineInterface.java
@@ -7,9 +7,16 @@ import marten.aoe.dto.MapDTO;
 import marten.aoe.dto.PlayerDTO;
 import marten.aoe.dto.PointDTO;
 import marten.aoe.engine.Engine;
+import marten.aoe.engine.EngineListener;
+import marten.aoe.engine.GlobalEvent;
+import marten.aoe.engine.LocalEvent;
 import marten.aoe.server.face.EngineFace;
 
+import org.apache.log4j.Logger;
+
 public class EngineInterface extends UnicastRemoteObject implements EngineFace {
+    private static org.apache.log4j.Logger log = Logger
+            .getLogger(EngineFace.class);
 
     private final Engine engine;
     private final PlayerDTO player;
@@ -30,14 +37,42 @@ public class EngineInterface extends UnicastRemoteObject implements EngineFace {
 
     @Deprecated
     @Override
-    public synchronized boolean moveUnit(PointDTO from, PointDTO to) {
+    public synchronized boolean moveUnit(PointDTO from, PointDTO to)
+            throws RemoteException {
         return this.engine.moveUnit(this.player, from, to);
     }
 
     @Deprecated
     @Override
-    public synchronized boolean createUnit(String name, PointDTO at) {
+    public synchronized boolean createUnit(String name, PointDTO at)
+            throws RemoteException {
         return this.engine.createUnit(this.player, name, at);
+    }
+
+    @Override
+    public void addListener() throws RemoteException {
+        this.engine.addListener(new EngineListener() {
+
+            @Override
+            public void onLocalEvent(LocalEvent event, PointDTO location) {
+                log.info(player.getName() + event + ", " + location);
+            }
+
+            @Override
+            public void onGlobalEvent(GlobalEvent event) {
+                log.info(player.getName() + event);
+            }
+        });
+    }
+
+    @Override
+    public void endTurn() throws RemoteException {
+        this.engine.endTurn(this.player);
+    }
+
+    @Override
+    public PlayerDTO getActivePlayer() throws RemoteException {
+        return this.engine.getActivePlayer();
     }
 
 }
