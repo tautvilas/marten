f99bcadcbb29bb1164b56efb130aadddf61ec084
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-03-06 17:09:17 +0000

    @NotNull annotation + aspect Documentation for annotations

--- trunk/aeonofechoes/src/main/java/marten/aoe/engine/util/NoNullArgs.java
+++ trunk/aeonofechoes/src/main/java/marten/aoe/engine/util/NoNullArgs.java
@@ -1,8 +1,18 @@
 package marten.aoe.engine.util;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * Methods and constructors marked with this annotation are decorated by aspectj
+ * aspect which checks if any of parameters is null and throws exception if this
+ * is true
+ */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
-//@Target(ElementType.FIELD)
-public @interface NoNullArgs {}
+@Target( { ElementType.METHOD, ElementType.CONSTRUCTOR })
+public @interface NoNullArgs {
+}
---
+++ trunk/aeonofechoes/src/main/java/marten/aoe/engine/util/NotNull.java
@@ -0,0 +1,19 @@
+package marten.aoe.engine.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Method or constructor parameters marked with this annotation are decorated by
+ * aspectj aspect which checks if the parameter is null and throws exception if
+ * that is true
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface NotNull {
+
+}
