ab60a1cb52979aec5d782d9e19852f688b99cd15
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-03-06 17:25:33 +0000

    Remove obsolete engine and tests.

--- trunk/aeonofechoes/src/main/java/marten/aoe/obsolete/engine/UnitProfileDatabaseListener.java
+++
@@ -1,6 +0,0 @@
-package marten.aoe.obsolete.engine;
-
-public interface UnitProfileDatabaseListener {
-    void onProfileAdded(UnitProfile unitProfile);
-    void onProfileRemoved(UnitProfile unitProfile);
-}
