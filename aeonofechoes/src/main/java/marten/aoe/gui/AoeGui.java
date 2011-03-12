39f6984ea65a2c80edcb68640cf11e34210abfbe
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-03-12 18:14:37 +0000

    Add custom mouse cursor functionality to AGE.
    Custom app cursor helps to avoid the uncanny feeling of mouse lag.

--- aeonofechoes/src/main/java/marten/aoe/gui/AoeGui.java
+++ aeonofechoes/src/main/java/marten/aoe/gui/AoeGui.java
@@ -1,6 +1,7 @@
 package marten.aoe.gui;
 
 import marten.age.core.AgeApp;
+import marten.aoe.Path;
 import marten.aoe.gui.scene.menu.MainMenu;
 
 public class AoeGui extends AgeApp {
@@ -8,6 +9,7 @@ public class AoeGui extends AgeApp {
     @Override
     public void configure() {
         this.setActiveScene(new MainMenu());
+        this.setCursor(Path.SKIN_DATA_PATH + "pointer.png");
     }
 
 }
