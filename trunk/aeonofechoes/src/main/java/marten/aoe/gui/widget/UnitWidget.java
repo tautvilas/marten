e1ebfa132889c74094b45c3b249ef3a56ec4ebc8
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-03-15 18:53:54 +0000

    Fancy unit movement points display.

--- trunk/aeonofechoes/src/main/java/marten/aoe/gui/widget/UnitWidget.java
+++ trunk/aeonofechoes/src/main/java/marten/aoe/gui/widget/UnitWidget.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import marten.age.graphics.appearance.Color;
 import marten.age.graphics.layout.BoxedObject;
 import marten.age.graphics.primitives.Dimension;
+import marten.age.graphics.primitives.Point;
 import marten.age.graphics.text.BitmapFont;
 import marten.age.graphics.text.BitmapString;
 import marten.age.graphics.text.FontCache;
@@ -15,15 +16,29 @@ import marten.aoe.dto.UnitDTO;
 public class UnitWidget extends TranslationGroup implements Widget, BoxedObject {
 
     private BitmapString representation;
-//    private UnitDTO unit;
+    private BitmapString movePoints;
+    // private UnitDTO unit;
     private final BitmapFont font = FontCache.getFont(new Font("Courier New",
             Font.BOLD, 20));
+    private final BitmapFont smallFont = FontCache.getFont(new Font(
+            "Courier New", Font.BOLD, 14));
 
     public UnitWidget(UnitDTO unit) {
-//        this.unit = unit;
-        this.representation = new BitmapString(font, unit.getName()
-                .charAt(0) + "", new Color(0.0, 1.0, 0.0));
+        // this.unit = unit;
+        this.representation = new BitmapString(font, unit.getName().charAt(0)
+                + "", new Color(0.0, 1.0, 0.0));
+        this.movePoints = new BitmapString(smallFont, unit
+                .getCurrentMovementPoints() + "");
+        float cp = unit.getCurrentMovementPoints();
+        float mp = unit.getMaximumMovementPoints();
+        if (cp == 0) {
+            this.movePoints.setColor(new Color(1, 0, 0));
+        } else {
+            this.movePoints.setColor(new Color(1 - cp / mp, cp / mp, 0));
+        }
+        this.movePoints.setPosition(new Point(-10, 10));
         this.addChild(representation);
+        this.addChild(movePoints);
     }
 
     @Override
