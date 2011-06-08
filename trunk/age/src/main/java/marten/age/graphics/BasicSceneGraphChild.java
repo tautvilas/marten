000afcddc9700c6d1aa509ef8c2bc4c238e7999d
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-06-08 18:21:25 +0000

    Extract NewMapDialog class to make MapEdito class lighter.

--- trunk/age/src/main/java/marten/age/graphics/BasicSceneGraphChild.java
+++ trunk/age/src/main/java/marten/age/graphics/BasicSceneGraphChild.java
@@ -2,7 +2,9 @@ package marten.age.graphics;
 
 public abstract class BasicSceneGraphChild implements SceneGraphChild {
     private BasicSceneGraphBranch<? extends SceneGraphChild> root = null;
-    
+
+    private boolean hidden = false;
+
     @Override
     public BasicSceneGraphBranch<? extends SceneGraphChild> getRoot() {
 	return this.root;
@@ -11,5 +13,20 @@ public abstract class BasicSceneGraphChild implements SceneGraphChild {
     @Override
     public void setRoot(BasicSceneGraphBranch<? extends SceneGraphChild> newRoot) {
 	this.root = newRoot;
+    }
+
+    @Override
+    public void show() {
+        this.hidden = false;
+    }
+
+    @Override
+    public void hide() {
+        this.hidden = true;
+    }
+
+    @Override
+    public boolean isHidden() {
+        return this.hidden;
     }
 }
