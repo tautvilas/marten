7f3cf36fb9182149f4d383c116207a4f16888800
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-09-09 17:23:16 +0000

    Multiple layer tabs in editor

--- aeonofechoes/src/main/java/marten/aoe/gui/scene/editor/MapEditor.java
+++ aeonofechoes/src/main/java/marten/aoe/gui/scene/editor/MapEditor.java
@@ -1,13 +1,19 @@
 package marten.aoe.gui.scene.editor;
 
 import java.awt.Font;
+import java.util.HashMap;
+import java.util.List;
 
 import marten.age.control.KeyboardController;
 import marten.age.control.MouseController;
 import marten.age.core.AgeScene;
 import marten.age.core.AppInfo;
+import marten.age.graphics.BasicSceneGraphBranch;
+import marten.age.graphics.SceneGraphBranch;
 import marten.age.graphics.flat.Flatland;
+import marten.age.graphics.geometry.primitives.Rectangle;
 import marten.age.graphics.layout.SimpleLayout;
+import marten.age.graphics.model.SimpleModel;
 import marten.age.graphics.primitives.Dimension;
 import marten.age.graphics.primitives.Point;
 import marten.age.graphics.text.BitmapFont;
@@ -15,9 +21,14 @@ import marten.age.graphics.text.FontCache;
 import marten.age.widget.Action;
 import marten.age.widget.Button;
 import marten.age.widget.obsolete.FpsCounter;
+import marten.aoe.dto.PointDTO;
+import marten.aoe.dto.TileDTO;
+import marten.aoe.gui.TileImageFactory;
+import marten.aoe.gui.TileLayer;
 import marten.aoe.gui.widget.AoeButtonFactory;
 import marten.aoe.gui.widget.MapWidget;
 import marten.aoe.gui.widget.Sidebar;
+import marten.aoe.gui.widget.TerrainCache;
 
 public class MapEditor extends AgeScene {
 
@@ -27,6 +38,7 @@ public class MapEditor extends AgeScene {
     private NewMapDialog newMapDialog;
     @SuppressWarnings("unused")
     private int mapSize = 0;
+    private HashMap<String, SceneGraphBranch<SimpleModel>> tabs = new HashMap<String, SceneGraphBranch<SimpleModel>>();
     private SimpleLayout layout = new SimpleLayout(AppInfo
             .getDisplayDimension());
 
@@ -70,14 +82,53 @@ public class MapEditor extends AgeScene {
         newMapDialog = new NewMapDialog(this, new Action() {
             @Override
             public void perform() {
-                MapWidget map = new MapWidget(
-                        newMapDialog.getMapSize(), AppInfo
-                                .getDisplayDimension());
+                MapWidget map = new MapWidget(newMapDialog.getMapSize(),
+                        AppInfo.getDisplayDimension());
                 map.setId("map");
                 MapEditor.this.flatland.updateChild(map, 0);
                 MapEditor.this.updateControllable(map.getId(), map);
             }
         });
+        // sidebar buttons
+        List<TileLayer> layers = TileImageFactory.getSortedLayerTypes();
+        String tab = "";
+        Point buttonPos = sidebar.getPosition().move(new Point(50, 500));
+        Point iconPos = null;
+        SceneGraphBranch<SimpleModel> layout = null;
+        for (TileLayer layer : layers) {
+            if (!tab.equals(layer.getPriorities()[0])) {
+                layout = new BasicSceneGraphBranch<SimpleModel>();
+                layout.hide();
+                tab = layer.getPriorities()[0];
+                iconPos = sidebar.getPosition().move(new Point(50, 450));
+                final Button button = AoeButtonFactory.getEditorTab(tab);
+                button.setPosition(buttonPos);
+                this.flatland.addChild(button);
+                this.flatland.addChild(layout);
+                buttonPos = buttonPos.move(new Point(50, 0));
+                this.tabs.put(tab, layout);
+                button.setAction(new Action() {
+                    @Override
+                    public void perform() {
+                        for (String key: MapEditor.this.tabs.keySet()) {
+                            SceneGraphBranch<SimpleModel> branch =MapEditor.this.tabs.get(key);
+                            if (key.equals(button.getId())) {
+                                branch.show();
+                            } else {
+                                branch.hide();
+                            }
+                        }
+                    }
+                });
+                this.registerControllable(button);
+            }
+            SimpleModel layerModel = TerrainCache.addType(new TileDTO(layer
+                    .getType(), new PointDTO(0, 0), null, true));
+            layerModel.addGeometry(new Rectangle(new Dimension(32, 32),
+                    iconPos));
+            iconPos = iconPos.move(new Point(0, -50));
+            layout.addChild(layerModel);
+        }
     }
 
     @Override
