6995b8b0f93592798c0c811a0497cc3fc8643996
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-09-12 12:09:17 +0000

    Additional constructors added to TileDTO

--- aeonofechoes/src/main/java/marten/aoe/dto/TileDTO.java
+++ aeonofechoes/src/main/java/marten/aoe/dto/TileDTO.java
@@ -5,25 +5,49 @@ import java.io.Serializable;
 public final class TileDTO implements Serializable {
     private static final long serialVersionUID = -1944860101763926870L;
     private final UnitDTO unit;
-    private final String name;
+    private final String[] layers;
     private final PointDTO point;
     private final boolean visible;
-    public TileDTO (String name, PointDTO point, UnitDTO unit, boolean visible) {
-        this.name = name;
+
+    public TileDTO(String[] layers, PointDTO point, UnitDTO unit) {
+        this(layers, point, unit, true);
+    }
+
+    public TileDTO(String name, PointDTO point, UnitDTO unit) {
+        this(new String[] {name}, point, unit, true);
+    }
+
+    public TileDTO(String name, PointDTO point, UnitDTO unit, boolean visible) {
+        this(new String[] {name}, point, unit, visible);
+    }
+
+    public TileDTO(String[] layers, PointDTO point, UnitDTO unit, boolean visible) {
+        if (layers.length == 0) {
+            throw new RuntimeException("Tile must have at least one layer");
+        }
+        this.layers = layers;
         this.unit = unit;
         this.point = point;
         this.visible = visible;
     }
-    public String getName () {
-        return this.name;
+
+    public String getName() {
+        return this.layers[0];
     }
-    public UnitDTO getUnit () {
+
+    public String[] getLayers() {
+        return this.layers;
+    }
+
+    public UnitDTO getUnit() {
         return this.unit;
     }
-    public PointDTO getCoordinates () {
+
+    public PointDTO getCoordinates() {
         return this.point;
     }
-    public boolean getVisibility () {
+
+    public boolean getVisibility() {
         return this.visible;
     }
 }
