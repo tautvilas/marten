e4b235aebfb92ba320a19ef2c7f8ae3b55da85de
mecinskas@gmail.com <mecinskas@gmail.com@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-09-17 14:03:43 +0000

    Use native mouse cursor image

--- aeonofechoes/src/main/java/marten/aoe/AoeGui.java
+++ aeonofechoes/src/main/java/marten/aoe/AoeGui.java
@@ -1,14 +1,17 @@
-package marten.aoe.gui;
+package marten.aoe;
 
 import marten.age.core.AgeApp;
 import marten.aoe.gui.scene.menu.MainMenu;
 
 public class AoeGui extends AgeApp {
+    public static void main(String[] args) {
+        new AoeGui().execute();
+    }
 
     @Override
     public void configure() {
         this.setActiveScene(new MainMenu());
-//        this.setCursor(Path.SKIN_DATA_PATH + "pointer.png");
+        this.setCursor(Path.SKIN_DATA_PATH + "pointer.png");
     }
 
 }
