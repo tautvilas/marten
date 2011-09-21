18e899177ff1ce303e99046bbcd32e4038e4e26c
mecinskas <mecinskas@673c011e-8dad-11dd-8ab4-67102e0efeef>
2011-09-21 12:11:54 +0000

    Added render to texture functionality to AGE

--- trunk/age/src/main/java/marten/age/graphics/texture/TextureLoader.java
+++ trunk/age/src/main/java/marten/age/graphics/texture/TextureLoader.java
@@ -3,11 +3,13 @@ package marten.age.graphics.texture;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
+import marten.age.graphics.SceneGraphChild;
 import marten.age.graphics.image.ImageData;
 import marten.age.graphics.image.ImageTransformations;
 import marten.age.graphics.primitives.Dimension;
 
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.EXTFramebufferObject;
 import org.lwjgl.opengl.GL11;
 
 public final class TextureLoader {
@@ -24,6 +26,54 @@ public final class TextureLoader {
         } else {
             return number;
         }
+    }
+
+    public static Texture loadTexture(SceneGraphChild graph, Dimension dimension) {
+        int width = (int)dimension.width;
+        int height = (int)dimension.height;
+
+        IntBuffer textureIdBuffer = BufferUtils.createIntBuffer(1);
+        GL11.glGenTextures(textureIdBuffer);
+        int textureId = textureIdBuffer.get(0);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureId);
+//        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER,
+//                GL11.GL_LINEAR);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER,
+                GL11.GL_LINEAR);
+        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height,
+                0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, (ByteBuffer)null);
+
+        IntBuffer buffer = BufferUtils.createIntBuffer(width * height);
+        EXTFramebufferObject.glGenFramebuffersEXT(buffer);
+        int id = buffer.get();
+        EXTFramebufferObject.glBindFramebufferEXT(
+                EXTFramebufferObject.GL_FRAMEBUFFER_EXT, id);
+
+        EXTFramebufferObject.glFramebufferTexture2DEXT(
+                EXTFramebufferObject.GL_FRAMEBUFFER_EXT,
+                EXTFramebufferObject.GL_COLOR_ATTACHMENT0_EXT,
+                GL11.GL_TEXTURE_2D, textureId, 0);
+
+        int framebuffer = EXTFramebufferObject
+                .glCheckFramebufferStatusEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT);
+
+        if (framebuffer != EXTFramebufferObject.GL_FRAMEBUFFER_COMPLETE_EXT) {
+            throw new RuntimeException(
+                    "Frame buffer is not complete, ogl code:" + framebuffer);
+        }
+
+        GL11.glPushAttrib(GL11.GL_VIEWPORT_BIT);
+        GL11.glViewport( 0, 0, width, height);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+        graph.render();
+
+        EXTFramebufferObject.glBindFramebufferEXT(
+                EXTFramebufferObject.GL_FRAMEBUFFER_EXT, 0);
+
+        GL11.glPopAttrib();
+
+        return new Texture(textureId, dimension);
     }
 
     public static Texture loadTexture(ImageData data) {
