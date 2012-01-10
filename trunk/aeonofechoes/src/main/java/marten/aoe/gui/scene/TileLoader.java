a225a5f502aa182341b1f8b13fb7d40f2bc4861e
mecinskas@gmail.com <mecinskas@gmail.com@673c011e-8dad-11dd-8ab4-67102e0efeef>
2012-01-10 18:45:53 +0000

    tile masking in action

--- trunk/aeonofechoes/src/main/java/marten/aoe/gui/scene/TileLoader.java
+++ trunk/aeonofechoes/src/main/java/marten/aoe/gui/scene/TileLoader.java
@@ -40,6 +40,7 @@ public abstract class TileLoader extends AgeScene {
             if (filename.charAt(0) == '.') continue;
             ImageCache.loadImage(files[i].getAbsolutePath(), "mask-" + filename.split("\\.")[0]);
         }
+
         ImageData mask1_1 = ImageCache.getImage("mask-1-1");
         ImageData mask1_2 = ImageCache.getImage("mask-1-2");
         ImageCache.addImage("msk-1-0", mask1_1);
@@ -48,6 +49,7 @@ public abstract class TileLoader extends AgeScene {
         ImageCache.addImage("msk-1-3", ImageTransformations.flip(mask1_1));
         ImageCache.addImage("msk-1-4", ImageTransformations.rotate(mask1_2, 2));
         ImageCache.addImage("msk-1-5", ImageTransformations.vflip(mask1_2));
+
         ImageData mask2_1 = ImageCache.getImage("mask-2-1");
         ImageData mask2_2 = ImageCache.getImage("mask-2-2");
         ImageCache.addImage("msk-2-0", mask2_2);
@@ -56,6 +58,7 @@ public abstract class TileLoader extends AgeScene {
         ImageCache.addImage("msk-2-3", ImageTransformations.rotate(mask2_2, 2));
         ImageCache.addImage("msk-2-4", ImageTransformations.flip(mask2_2));
         ImageCache.addImage("msk-2-5", mask2_1);
+
         ImageData mask3_1 = ImageCache.getImage("mask-3-1");
         ImageData mask3_2 = ImageCache.getImage("mask-3-2");
         ImageCache.addImage("msk-3-0", ImageTransformations.rotate(mask3_2, 2));
@@ -64,7 +67,7 @@ public abstract class TileLoader extends AgeScene {
         ImageCache.addImage("msk-3-3", mask3_2);
         ImageCache.addImage("msk-3-4", mask3_1);
         ImageCache.addImage("msk-3-5", ImageTransformations.vflip(mask3_2));
-        ImageData mask6 = ImageCache.getImage("mask-6");
+
         ImageData msk_2_0 = ImageCache.getImage("msk-2-0");
         ImageData msk_2_1 = ImageCache.getImage("msk-2-1");
         ImageData msk_2_2 = ImageCache.getImage("msk-2-2");
@@ -77,6 +80,17 @@ public abstract class TileLoader extends AgeScene {
         ImageCache.addImage("msk-4-3", ImageTransformations.blend(msk_2_3, msk_2_1));
         ImageCache.addImage("msk-4-4", ImageTransformations.blend(msk_2_4, msk_2_2));
         ImageCache.addImage("msk-4-5", ImageTransformations.blend(msk_2_5, msk_2_3));
+
+        ImageData mask_5_1 = ImageCache.getImage("mask-5-1");
+        ImageData mask_5_2 = ImageCache.getImage("mask-5-2");
+        ImageCache.addImage("msk-5-0", ImageTransformations.flip(mask_5_2));
+        ImageCache.addImage("msk-5-1", mask_5_2);
+        ImageCache.addImage("msk-5-2", ImageTransformations.flip(mask_5_1));
+        ImageCache.addImage("msk-5-3", ImageTransformations.vflip(mask_5_2));
+        ImageCache.addImage("msk-5-4", ImageTransformations.rotate(mask_5_2, 2));
+        ImageCache.addImage("msk-5-5", mask_5_1);
+
+        ImageData mask6 = ImageCache.getImage("mask-6");
         ImageCache.addImage("msk-6", mask6);
     }
 }
