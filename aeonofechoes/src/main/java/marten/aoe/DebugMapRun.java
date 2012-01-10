d41ae9d3b73c7ead110aeff5e22c7407f3aacb50
mecinskas@gmail.com <mecinskas@gmail.com@673c011e-8dad-11dd-8ab4-67102e0efeef>
2012-01-10 19:47:14 +0000

    Create quick-launch class for debug map for quicker testing

---
+++ aeonofechoes/src/main/java/marten/aoe/DebugMapRun.java
@@ -0,0 +1,22 @@
+package marten.aoe;
+
+import marten.age.core.AgeApp;
+import marten.aoe.gui.scene.GameGate;
+
+public class DebugMapRun extends AgeApp {
+
+    public DebugMapRun(String title, boolean show) {
+        super(title, show);
+    }
+
+    public static void main(String[] args) {
+        new DebugMapRun("2p debug map", false).execute();
+    }
+
+    @Override
+    public void configure() {
+        GameInfo.nickname = "zvitruolis";
+        this.setActiveScene(new GameGate("2pDebug"));
+        this.setCursor(Path.SKIN_DATA_PATH + "pointer.png");
+    }
+}
