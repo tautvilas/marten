930948dfcf20fd1a8f2f87a39db66479e1129f60
mecinskas@gmail.com <mecinskas@gmail.com@673c011e-8dad-11dd-8ab4-67102e0efeef>
2012-01-29 13:00:46 +0000

    More unit refactoring

--- trunk/aeonofechoes/src/main/java/marten/aoe/data/units/UnitDetails.java
+++ trunk/aeonofechoes/src/main/java/marten/aoe/data/units/UnitDetails.java
@@ -1,7 +1,89 @@
 package marten.aoe.data.units;
 
+import marten.aoe.data.type.UnitSize;
+import marten.aoe.data.type.UnitType;
+import marten.aoe.dto.depreciated.DamageDTO;
+
 public class UnitDetails {
+
+    private UnitSize unitSize = UnitSize.MEDIUM;
+    private UnitType unitType = UnitType.GROUND;
+    private int maxMovementAllowance = 10;
+    private int maxHitPoints = 10;
+    private DamageDTO meleeDamage;
+    private DamageDTO rangedDamage;
+    private int attackRange;
+    private int detectionRange= 5;
     private String id;
+
+    public UnitSize getUnitSize() {
+        return unitSize;
+    }
+
+    public void setUnitSize(UnitSize unitSize) {
+        this.unitSize = unitSize;
+    }
+
+    public UnitType getUnitType() {
+        return unitType;
+    }
+
+    public void setUnitType(UnitType unitType) {
+        this.unitType = unitType;
+    }
+
+    public int getMaxMovementAllowance() {
+        return maxMovementAllowance;
+    }
+
+    public void setMaxMovementAllowance(int maxMovementAllowance) {
+        this.maxMovementAllowance = maxMovementAllowance;
+    }
+
+    public int getMaxHitPoints() {
+        return maxHitPoints;
+    }
+
+    public void setMaxHitPoints(int maxHitPoints) {
+        this.maxHitPoints = maxHitPoints;
+    }
+
+    public DamageDTO getMeleeDamage() {
+        return meleeDamage;
+    }
+
+    public void setMeleeDamage(DamageDTO meleeDamage) {
+        this.meleeDamage = meleeDamage;
+    }
+
+    public DamageDTO getRangedDamage() {
+        return rangedDamage;
+    }
+
+    public void setRangedDamage(DamageDTO rangedDamage) {
+        this.rangedDamage = rangedDamage;
+    }
+
+    public int getAttackRange() {
+        return attackRange;
+    }
+
+    public void setAttackRange(int attackRange) {
+        this.attackRange = attackRange;
+    }
+
+    public int getDetectionRange() {
+        return detectionRange;
+    }
+
+    public void setDetectionRange(int detectionRange) {
+        this.detectionRange = detectionRange;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public UnitDetails(String id) {
         this.id = id;
     }
