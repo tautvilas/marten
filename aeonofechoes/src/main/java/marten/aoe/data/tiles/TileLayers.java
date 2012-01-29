92e75d6cf90aad2889e7b0ec158561b0e9c8ccde
mecinskas@gmail.com <mecinskas@gmail.com@673c011e-8dad-11dd-8ab4-67102e0efeef>
2012-01-29 10:10:15 +0000

    add water type

---
+++ aeonofechoes/src/main/java/marten/aoe/data/tiles/TileLayers.java
@@ -0,0 +1,6 @@
+package marten.aoe.data.tiles;
+
+public class TileLayers {
+    public static final String WATER = "Water";
+    public static final String CONCRETE = "Concrete";
+}
--- aeonofechoes/src/main/java/marten/aoe/dto/TileLayerDTO.java
+++ aeonofechoes/src/main/java/marten/aoe/dto/TileLayerDTO.java
@@ -5,12 +5,22 @@ import java.io.Serializable;
 public class TileLayerDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private TileType type;
     private int groundMovementCost;
     private String name;
 
+    public TileLayerDTO(String name) {
+        this(name, 1);
+    }
+
     public TileLayerDTO(String name, int groundMovementCost) {
+        this(name, groundMovementCost, TileType.GROUND);
+    }
+
+    public TileLayerDTO(String name, int groundMovementCost, TileType type) {
         this.name = name;
         this.groundMovementCost = groundMovementCost;
+        this.type = type;
     }
 
     public String getName() {
@@ -19,6 +29,10 @@ public class TileLayerDTO implements Serializable {
 
     public int getGroundMovementCost() {
         return this.groundMovementCost;
+    }
+
+    public TileType getType() {
+        return this.type;
     }
 
 }
