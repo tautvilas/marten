1c5a783eba691adb9d02c715aa0b16cd86163a07
mecinskas@gmail.com <mecinskas@gmail.com@673c011e-8dad-11dd-8ab4-67102e0efeef>
2012-02-05 19:58:28 +0000

    Neat unit movement animation

--- aeonofechoes/src/main/java/marten/aoe/gui/UnitDrawer.java
+++ aeonofechoes/src/main/java/marten/aoe/gui/UnitDrawer.java
@@ -26,47 +26,72 @@ public class UnitDrawer {
         this.context = context;
     }
 
-    public void updateTile(TileDTO oldTile, TileDTO tile, Point tileDisplayCoordinates) {
+    public void updateTile(TileDTO oldTile, TileDTO tile,
+            Point tileDisplayCoordinates) {
         UnitDTO unitDTO = tile.getUnit();
-        UnitDTO oldUnit = null;
-        if (oldTile != null) {
-            oldUnit = oldTile.getUnit();
-        }
+//        UnitDTO oldUnit = null;
+//        if (oldTile != null) {
+//            oldUnit = oldTile.getUnit();
+//        }
         PointDTO coords = tile.getCoordinates();
         if (unitDTO != null) {
             int id = unitDTO.getId();
             if (!this.units.containsKey(id)) {
                 UnitWidget unit = new UnitWidget(tile.getUnit());
                 unit.setPosition(new Point(tileDisplayCoordinates.x + 64 / 2
-                        - unit.getDimension().width / 2, tileDisplayCoordinates.y
-                        + 64 / 2 - unit.getDimension().height / 2));
+                        - unit.getDimension().width / 2,
+                        tileDisplayCoordinates.y + 64 / 2
+                                - unit.getDimension().height / 2));
                 this.context.addChild(unit);
-                this.units.put(tile.getUnit().getId(), new UnitData(unit, coords));
+                this.units.put(tile.getUnit().getId(), new UnitData(unit,
+                        coords));
                 log.info("Unit " + id + " entered " + coords);
             } else {
                 UnitData unit = this.units.get(id);
-                unit.widget.setPosition(new Point(tileDisplayCoordinates.x + 64 / 2
-                        - unit.widget.getDimension().width / 2, tileDisplayCoordinates.y
-                        + 64 / 2 - unit.widget.getDimension().height / 2));
+                unit.path.add(new Point(tileDisplayCoordinates.x + 64 / 2
+                        - unit.widget.getDimension().width / 2,
+                        tileDisplayCoordinates.y + 64 / 2
+                                - unit.widget.getDimension().height / 2));
                 unit.widget.update(unitDTO);
-                unit.path.add(coords);
+                unit.position = coords;
                 log.info("Unit " + id + " moved to " + coords);
             }
-        } else if (oldUnit != null){
+        }/* else if (oldUnit != null) {
+            log.info("Deleting unit");
             UnitData unit = this.units.get(oldUnit.getId());
-            if (unit.path.get(unit.path.size() - 1).equals(coords)) {
+            if (unit.position.equals(coords)) {
                 this.context.removeChild(unit.widget);
                 this.units.remove(oldUnit.getId());
+            }
+        } */
+    }
+
+    public void animate(int speed) {
+        for (UnitData unit : this.units.values()) {
+            if  (!unit.path.isEmpty()) {
+                Point nextPos = unit.path.get(0);
+                Point pos = unit.widget.getPosition();
+                if (nextPos.approximatelyEquals(pos, speed)) {
+                    unit.widget.setPosition(nextPos);
+                    unit.path.remove(0);
+                } else {
+                    int dx = Integer.signum((int)(nextPos.x - pos.x));
+                    int dy = Integer.signum((int)(nextPos.y - pos.y));
+                    Point newPos = new Point(pos.x + dx * speed, pos.y + dy * speed);
+                    unit.widget.setPosition(newPos);
+                }
             }
         }
     }
 
     private class UnitData {
         public UnitWidget widget;
-        public List<PointDTO> path = new ArrayList<PointDTO>();
-        public UnitData(UnitWidget widget, PointDTO coords) {
-            path.add(coords);
+        public List<Point> path = new ArrayList<Point>();
+        public PointDTO position;
+
+        public UnitData(UnitWidget widget, PointDTO position) {
             this.widget = widget;
+            this.position = position;
         }
     }
 }
